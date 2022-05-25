3f24bfe12cc34da2e475204643d4c2603f64fa5c
De333get <de333get@mail.ru>
2022-05-25 18:59:17 +0300

    fix: with ReadStrim

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, 'text.txt')
-fs.readFile(filePath, 'utf-8', (err, content) => {
-	if (err) {
-		throw err
-	}
-	console.log(content)
-})
+const read = fs.createReadStream(path.join(__dirname, 'text.txt'));
+read.on('data', chunk => console.log(chunk.toString()));
